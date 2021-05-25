0809085503147cefb6007219e1e5d929d1ba96f9
Gabriel Forsythe <gabe.forsythe@gmail.com>
2021-05-24 17:04:09 -0700

    finally added the last piece of the pokedex. Searchbar functionality works but only for lowercase

--- pokedex/src/components/Pokedex.js
+++ pokedex/src/components/Pokedex.js
@@ -8,13 +8,15 @@ import {
   CardMedia,
   CircularProgress,
   Typography,
+  TextField,
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { fade, makeStyles } from '@material-ui/core/styles';
 import { upperCase } from "../helpers/upperCase";
+import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   pokedexContainer: {
     paddingTop: "40px",
     paddingLeft: "50px",
@@ -28,7 +30,29 @@ const useStyles = makeStyles({
   cardContent: {
     textAlign: "center",
   },
-});
+
+  searchContainer: {
+    display: "flex",
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    paddingLeft: "20px",
+    paddingRight: "20px",
+    marginTop: "5px",
+    marginBottom: "5px",
+
+  },
+
+  searchIcon: {
+    alignSelf: "flex-end",
+    marginBottom: "5px",
+  },
+
+  searchInput: {
+    width: "200px",
+    margin: "5px"
+  }
+
+
+}));
 
 
 
@@ -37,24 +61,29 @@ const useStyles = makeStyles({
 export default function Pokedex(props) {
   const { history } = props;
   const [pokemonData, setPokemonData] = useState({});
+  const [filter, setFilter] = useState("");
   const classes = useStyles();
+
+  const handleSearchChange = (e) => {
+    setFilter(e.target.value);
+  };
 
   useEffect(() => {
     axios.get(`https://pokeapi.co/api/v2/pokemon?limit=898`)
-    .then(function(response){
-      const {data} = response;
-      const {results} = data;
-      const newPokemonData = {};
-      results.forEach((pokemon, index) => {
-        newPokemonData[index + 1] = {
-          id: index + 1,
-          name: pokemon.name,
-          sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
-        }
-      })
-      setPokemonData(newPokemonData)
+      .then(function (response) {
+        const { data } = response;
+        const { results } = data;
+        const newPokemonData = {};
+        results.forEach((pokemon, index) => {
+          newPokemonData[index + 1] = {
+            id: index + 1,
+            name: pokemon.name,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+          };
+        });
+        setPokemonData(newPokemonData);
 
-    });
+      });
   }, []);
 
   const getPokemonCard = (pokemonId) => {
@@ -77,11 +106,19 @@ export default function Pokedex(props) {
   return (
     <>
       <AppBar position="static">
-        <Toolbar></Toolbar>
+        <Toolbar>
+          <div className={classes.searchContainer}>
+            <SearchIcon className={classes.searchIcon} />
+            <TextField className={classes.searchInput} onChange={handleSearchChange}
+              label="Pokemon" variant="standard" />
+          </div>
+        </Toolbar>
       </AppBar>
       {pokemonData ? (
         <Grid container spacing={2} className={classes.pokedexContainer}>
-          {Object.keys(pokemonData).map(pokemonId => getPokemonCard(pokemonId))}
+          {Object.keys(pokemonData).map(pokemonId =>
+            pokemonData[pokemonId].name.includes(filter) &&
+            getPokemonCard(pokemonId))}
         </Grid>
       ) : (
         <CircularProgress></CircularProgress>
